ec39f5135d848c7ee62f4b5205512a15591a7205
Eben <986519126@qq.com>
2021-03-13 15:20:10 +0800

    Update main.py

--- homeworkA/main.py
+++ homeworkA/main.py
@@ -2,18 +2,19 @@ from grid import Grid
 import numpy as np
 
 # state-values predicted by first view Monte Carlo
+GAMMA = 0.9
 play = Grid()
 iterations = 100000
 values = np.zeros((4, 4))
 returns = {(i, j): list() for i in range(4) for j in range(4)}
-gamma = 0.9
+
 
 for a in range(iterations):
     episode = play.generate_episode()
     G = 0
     for i, step in enumerate(episode[::-1]):
         # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
-        G = gamma*G + step[2]
+        G = GAMMA * G + step[2]
         if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
             idx = (step[0][0], step[0][1])  # idx is (x, y)
             returns[idx].append(G)
