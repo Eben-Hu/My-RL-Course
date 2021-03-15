95b98ee59ff7faa9cf12f635fd921ffcd41b37a6
Eben <986519126@qq.com>
2021-03-15 20:09:47 +0800

    Update main.py

--- homeworkA/main.py
+++ homeworkA/main.py
@@ -4,6 +4,8 @@ import numpy as np
 # (2) state-values predicted by first view Monte Carlo
 GAMMA = 0.9
 GAMMA1 = 1
+ALPHA = 0.1
+Epsilon = 0.1
 # state-values predicted by first view Monte Carlo
 play = Grid()
 episodes = 100000
@@ -31,20 +33,37 @@ returns = {(i, j): list() for i in range(4) for j in range(4)}
 # print(values)
 
 # (3) evaluate the policy performance R_Avg
-for a in range(episodes):
-    episode = play.generate_episode()
-    G = 0
-    for i, step in enumerate(episode[::-1]):
-        # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
-        G = GAMMA1 * G + step[2]
-        if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
-            idx = (step[0][0], step[0][1])  # idx is (x, y)
-            returns[idx].append(G)
-            temp_values = np.average(returns[idx])
-            values[idx[0], idx[1]] = temp_values
-        # show the process every 5000 iterations
-    if a % 5000 == 0:
-        print(f'In the No.{a} episodes, the policy performance R_Avg is {sum(sum(values))/15}.')
+# for a in range(episodes):
+#     episode = play.generate_episode()
+#     G = 0
+#     for i, step in enumerate(episode[::-1]):
+#         # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
+#         G = GAMMA1 * G + step[2]
+#         if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
+#             idx = (step[0][0], step[0][1])  # idx is (x, y)
+#             returns[idx].append(G)
+#             temp_values = np.average(returns[idx])
+#             values[idx[0], idx[1]] = temp_values
+#         # show the process every 5000 iterations
+#     if a % 5000 == 0:
+#         print(f'In the No.{a} episodes, the policy performance R_Avg is {sum(sum(values))/15}.')
+#
+# # the final state value functions
+# print(f'The final policy performance R_Avg is {sum(sum(values))/15}:')
 
-# the final state value functions
-print(f'The final policy performance R_Avg is {sum(sum(values))/15}:')
+# （4） the optimal tabular policy with Q-learning and its policy performance R_Avg
+q_tables = 
+
+
+def generate_episode(self):
+    # this function is for Monte Carlo to generate episodes
+    initial_state = random.choice(self.states[:-1])
+    episode = []  # episode contains [s0, action0, reward1,s1, s1, action1,...sT-1, actionT-1, final_reward, sT]
+    while True:
+        if list(initial_state) in self.terminate_state:
+            return episode
+        else:
+            action = self.get_action()
+            final_state, current_reward = self.step(initial_state, action)
+            episode.append([list(initial_state), action, current_reward, list(final_state)])
+            initial_state = final_state
