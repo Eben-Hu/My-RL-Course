7b15a52952fb7e34eabe5e68766319682739ae20
Eben Hu <EbenHu@outlook.com>
2021-03-17 21:31:45 +0800

    Update grid.py

--- homeworkA/grid.py
+++ homeworkA/grid.py
@@ -14,6 +14,8 @@ class Grid:
         self.neighbor_actions1 = {'up': 'right', 'down': 'left', 'right': 'down', 'left': 'up'}
         self.neighbor_actions2 = {'up': 'left', 'down': 'right', 'right': 'up', 'left': 'down'}
         self.discount = 0.9
+        self.epsilon = 0.1
+        self.alpha = 0.01  # learning rate
         self.final_reward = 9
         self.normal_reward = -1
 
@@ -64,3 +66,41 @@ class Grid:
                 final_state, current_reward = self.step(initial_state, action)
                 episode.append([list(initial_state), action, current_reward, list(final_state)])
                 initial_state = final_state
+
+    def pair_to_sequence(self, state):
+        return state[0]*4+state[1]
+
+    def q_learning(self, q_tables):
+        initial_state = random.choice(self.states[:-1])  # (x, y)
+        episode = []
+        while True:
+            if list(initial_state) in self.terminate_state:
+                return episode
+            else:
+                p = np.random.random()
+                initial_sequence = self.pair_to_sequence(initial_state)  # initial_sequence = St
+                if p < self.epsilon/4:  # epsilon
+                    action = self.get_action()  # action = At, randomly selected
+                    action_index = self.actions.index(action)
+                    final_state, current_reward = self.step(initial_state, action)  # final_state = St+1, Rt+1
+                    final_sequence = self.pair_to_sequence(final_state)
+                    episode.append([list(initial_state), action, current_reward, list(final_state)])
+                    next_action_index = np.argmax(q_tables[final_sequence])
+                    next_action = self.actions_pairs[self.actions[next_action_index]]  # next_action = At+1
+                    q_tables[initial_sequence][action_index] = q_tables[initial_sequence][action_index] + self.alpha *(current_reward + self.discount * (q_tables[final_sequence][next_action_index] - q_tables[initial_sequence][action_index]))
+                    initial_state = final_state
+                else:
+                    action_index = np.argmax(q_tables[initial_sequence])  # action = At
+                    action = self.actions[action_index]  # 'up'/'down'/'left'/'right'
+                    final_state, current_reward = self.step(initial_state, action)  # final_state = St+1, Rt+1
+                    final_sequence = self.pair_to_sequence(final_state)
+                    episode.append([list(initial_state), action, current_reward, list(final_state)])
+                    next_action_index = np.argmax(q_tables[final_sequence])
+                    next_action = self.actions_pairs[self.actions[next_action_index]]  # next_action = At+1
+                    q_tables[initial_sequence][action_index] = q_tables[initial_sequence][action_index] + self.alpha * (current_reward + self.discount * (q_tables[final_sequence][next_action_index] - q_tables[initial_sequence][action_index]))
+                    initial_state = final_state
+        return q_tables
+
+
+
+
