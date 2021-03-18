4e87d37b1591b92d7bf52dab6e23b8ac9f5a5c52
Eben Hu <EbenHu@outlook.com>
2021-03-18 15:00:54 +0800

    Create main.py

---
+++ homeworkA/old_version/main.py
@@ -0,0 +1,69 @@
+from homeworkA.old_version.grid import Grid
+import numpy as np
+
+# (2) state-values predicted by first view Monte Carlo
+GAMMA = 0.9
+GAMMA1 = 1
+ALPHA = 0.1
+Epsilon = 0.1
+# state-values predicted by first view Monte Carlo
+play = Grid()
+episodes = 100000
+values = np.zeros((4, 4))
+returns = {(i, j): list() for i in range(4) for j in range(4)}
+
+
+# for a in range(episodes):
+#     episode = play.generate_episode()
+#     G = 0
+#     for i, step in enumerate(episode[::-1]):
+#         # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
+#         G = GAMMA * G + step[2]
+#         if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
+#             idx = (step[0][0], step[0][1])  # idx is (x, y)
+#             returns[idx].append(G)
+#             temp_values = np.average(returns[idx])
+#             values[idx[0], idx[1]] = temp_values
+#     # show the process every 5000 iterations
+#     if a % 5000 == 0:
+#         print(f'This is the No.{a} iterations.')
+#         print(values)
+#
+# # the final state value functions
+# print('The final state values are:')
+# print(values)
+
+# (3) evaluate the policy performance R_Avg
+# for a in range(episodes):
+#     episode = play.generate_episode()
+#     G = 0
+#     for i, step in enumerate(episode[::-1]):
+#         # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
+#         G = GAMMA1 * G + step[2]
+#         if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
+#             idx = (step[0][0], step[0][1])  # idx is (x, y)
+#             returns[idx].append(G)
+#             temp_values = np.average(returns[idx])
+#             values[idx[0], idx[1]] = temp_values
+#         # show the process every 5000 iterations
+#     if a % 5000 == 0:
+#         print(f'In the No.{a} episodes, the policy performance R_Avg is {sum(sum(values))/15}.')
+#
+# # the final state value functions
+# print(f'The final policy performance R_Avg is {sum(sum(values))/15}:')
+
+# （4） the optimal tabular policy with Q-learning and its policy performance R_Avg
+def extraction(q_table):
+    policy_list = []
+    for row in range(len(q_table)):
+        policy_list.append(actions[np.argmax(q_table[row])])
+    print(np.array(policy_list).reshape(4, 4))
+
+
+q_tables = np.zeros((16, 4), dtype=np.float) + np.random.normal(0, 0.3, (16, 4))
+q_tables[15][:] = 0
+actions = ['up', 'down', 'left', 'right']
+for episode in range(10000):
+    _, q = play.q_learning(q_tables)
+print(q)
+extraction(q)
