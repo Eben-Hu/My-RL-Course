c43b864fe6ecc6907ed4e06b315e5b87762a9816
Eben Hu <EbenHu@outlook.com>
2021-03-19 00:03:30 +0800

    Update gridworld.py

--- homeworkA/gridworld.py
+++ homeworkA/gridworld.py
@@ -17,34 +17,37 @@ class GridWorld(gym.Env, ABC):
         self.normal_reward = normal_reward
         self.final_reward = final_reward
         self.end = 15  # (3, 3)
-        self.action_pairs = {0: -4, 1: 4, 2: -1, 3: 1}
+        self.action_pairs = {0: -4, 1: 4, 2: -1, 3: 1}  # {up:-4, down:4, left:-1, right:1}
+        self.actions_match = {-4: np.array([-1, 0]), 4: np.array([1, 0]), -1: np.array([0, -1]),
+                              1: np.array([0, 1])}
         self.neighbor_actions1 = {-4: 1, 4: -1, -1: -4, 1: 4}  # {up:right, down:left, left:up, right:down}
         self.neighbor_actions2 = {-4: -1, 4: 1, -1: 4, 1: -4}    # {up:left, down:right, left:down, right:up}
 
     def reset(self):
         self.obs = np.random.randint(0, self.n_width * self.n_height - 1)
-        self.row = self.obs // self.n_width
-        self.column = self.obs % self.n_height
         return self.obs
 
     def step(self, action):  # action = -4, +4, -1 +1 / up, down, left, right, action is an int number
         done = 0
         info = {}
         p = np.random.random()  # the random behavior
-        if p < 0.8:
+        if p < 0.8:  # decide the real action
             self.action = action
         elif p < 0.9:
             self.action = self.neighbor_actions1[action]
         else:
             self.action = self.neighbor_actions2[action]
-        temp = self.obs + self.action
-        if temp == self.end:  # 15
+        self.row = self.obs // self.n_width  # x
+        self.column = self.obs % self.n_height  # y
+        obs = np.array([self.row, self.column])  # old state/obs [x, y]
+        temp = obs + self.actions_match[self.action]  # new obs [x, y]
+        if temp[0] * 4 + temp[1] == self.end:  # final state
             self.obs = self.end
             reward = self.final_reward
             done = 1
-        elif temp in range(self.n_width * self.n_height - 1):  # 0~14
-            self.obs = temp
+        elif temp[0] < 0 or temp[0] > 3 or temp[1] < 0 or temp[1] > 3:  # out of the range
             reward = self.normal_reward
-        else:  # out of the range(16)
+        else:
+            self.obs = temp[0] * 4 + temp[1]
             reward = self.normal_reward
         return self.obs, reward, done, info
