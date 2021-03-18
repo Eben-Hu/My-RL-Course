10a2a559444977bd6e68d6baf90056b6ce804558
Eben Hu <EbenHu@outlook.com>
2021-03-18 17:29:12 +0800

    Update gridworld.py

--- homeworkA/gridworld.py
+++ homeworkA/gridworld.py
@@ -13,25 +13,31 @@ class GridWorld(gym.Env, ABC):
         self.len = n_width * n_height
         self.action_space = gym.spaces.Discrete(4)  # {0:up} {1:down} {2:left} {3:right}
         self.observation_space = gym.spaces.Discrete(self.n_height * self.n_width)
-        self.obs = None
-        self.row = self.obs // self.n_width
-        self.column = self.obs % self.n_height
         self.action = None
         self.normal_reward = normal_reward
         self.final_reward = final_reward
         self.end = 15  # (3, 3)
         self.action_pairs = {0: -4, 1: 4, 2: -1, 3: 1}
-        self.neighbor_actions1 = {0: 3, 1: 2, 2: 2, 3: 0}
-        self.neighbor_actions2 = {0: 2, 1: 3, 2: 0, 3: 2}
+        self.neighbor_actions1 = {-4: 1, 4: -1, -1: -4, 1: 4}  # {up:right, down:left, left:up, right:down}
+        self.neighbor_actions2 = {-4: -1, 4: 1, -1: 4, 1: -4}    # {up:left, down:right, left:down, right:up}
 
     def reset(self):
-        self.obs = np.random.randint(range(0, self.n_width * self.n_height))
+        self.obs = np.random.randint(0, self.n_width * self.n_height - 1)
+        self.row = self.obs // self.n_width
+        self.column = self.obs % self.n_height
         return self.obs
 
-    def step(self, action):  # action = {up:-4} {down:+4} {left:-1} {right:+1}
+    def step(self, action):  # action = -4, +4, -1 +1 / up, down, left, right, action is an int number
         done = 0
         info = {}
-        temp = self.obs + action
+        p = np.random.random()  # the random behavior
+        if p < 0.8:
+            self.action = action
+        elif p < 0.9:
+            self.action = self.neighbor_actions1[action]
+        else:
+            self.action = self.neighbor_actions2[action]
+        temp = self.obs + self.action
         if temp == self.end:  # 15
             self.obs = self.end
             reward = self.final_reward
@@ -42,14 +48,3 @@ class GridWorld(gym.Env, ABC):
         else:  # out of the range(16)
             reward = self.normal_reward
         return self.obs, reward, done, info
-
-    def random_action(self):
-        random_action = self.action_space.sample()
-        p = np.random.random()
-        if p < 0.8:
-            decided_action = random_action
-        elif p < 0.9:
-            decided_action = self.neighbor_actions1[random_action]
-        else:
-            decided_action = self.neighbor_actions2[random_action]
-        return self.action_pairs[decided_action]
