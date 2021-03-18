027945df14371979b568d6fc8516f859d9770e4c
Eben Hu <EbenHu@outlook.com>
2021-03-18 19:34:09 +0800

    Update utilities.py

--- homeworkA/utilities.py
+++ homeworkA/utilities.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 class Get_Action:
+    def __init__(self):
+        self.EPSILON = 0.1
+
     def stochastic(self, obs=None):
         action = np.random.choice([-4, 4, -1, 1])
         return action
 
-    def 
+
 
 
 
