791ebb82683527b3b50c9f19fb449c1de09add28
Eben Hu <EbenHu@outlook.com>
2021-03-19 17:48:56 +0800

    Update main.py

--- homeworkA/main.py
+++ homeworkA/main.py
@@ -6,13 +6,13 @@ values = first_visit_monte_carlo_evaluate()
 print(f'The final values are:{values.reshape(4, 4)}')
 
 # (3)  evaluate the policy performance R_Avg
-values = first_visit_monte_carlo_evaluate(discount=1.0)
-print(f'The final policy performance R_Avg is {sum(values)/15}.')
-
-# (4) the optimal tabular policy with Q-learning and its policy performance R_Avg
-q_tables = np.zeros((16, 4), dtype=np.float) + np.random.normal(0, 0.3, (16, 4))
-q_tables[15][:] = 0
-final_q_tables, final_policy = q_learning(q_tables)
-print(final_q_tables)
-print(final_policy)
+# values = first_visit_monte_carlo_evaluate(discount=1.0)
+# print(f'The final policy performance R_Avg is {sum(values)/15}.')
+#
+# # (4) the optimal tabular policy with Q-learning and its policy performance R_Avg
+# q_tables = np.zeros((16, 4), dtype=np.float) + np.random.normal(0, 0.3, (16, 4))
+# q_tables[15][:] = 0
+# final_q_tables, final_policy = q_learning(q_tables)
+# print(final_q_tables)
+# print(final_policy)
 
