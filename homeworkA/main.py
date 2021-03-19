2bcd1439788a2a3083f151687a1b60845f40f9c7
Eben Hu <EbenHu@outlook.com>
2021-03-19 11:46:48 +0800

    Update main.py

--- homeworkA/main.py
+++ homeworkA/main.py
@@ -3,12 +3,12 @@ from policies import first_visit_monte_carlo_evaluate, q_learning
 import numpy as np
 
 # (2)  state-values evaluated by first view Monte Carlo
-# values = first_visit_monte_carlo_evaluate()
-# print(f'The final values are:{values.reshape(4, 4)}')
+values = first_visit_monte_carlo_evaluate()
+print(f'The final values are:{values.reshape(4, 4)}')
 
 # (3)  evaluate the policy performance R_Avg
-# values = first_visit_monte_carlo_evaluate(discount=1.0)
-# print(f'The final policy performance R_Avg is {sum(values)/15}.')
+values = first_visit_monte_carlo_evaluate(discount=1.0)
+print(f'The final policy performance R_Avg is {sum(values)/15}.')
 
 # (4) the optimal tabular policy with Q-learning and its policy performance R_Avg
 q_tables = np.zeros((16, 4), dtype=np.float) + np.random.normal(0, 0.3, (16, 4))
