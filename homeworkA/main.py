4fa0147c19a9b6dd2778afaa82c022d2514d6217
Eben <986519126@qq.com>
2021-03-19 23:28:07 +0800

    Update main.py

--- homeworkA/main.py
+++ homeworkA/main.py
@@ -9,7 +9,7 @@ def monte_carlo():
 
 
 # (3)  evaluate the policy performance R_Avg
-def performance():
+def monte_carlo_performance():
     values = first_visit_monte_carlo_evaluate(gamma=1.0)
     print(f'The final policy performance R_Avg is {sum(values) / 15}.')
 
@@ -18,10 +18,10 @@ def performance():
 def qlearning():
     q_tables = np.zeros((16, 4), dtype=np.float) + np.random.normal(0, 0.3, (16, 4))
     q_tables[15][:] = 0
-    final_q_tables, final_policy = q_learning(q_tables)
-    print(final_q_tables)
-    print(final_policy)
+    final_q_tables, final_policy, final_performance = q_learning(q_tables)
+    print(f'The final q tables is :{final_q_tables} with the final optimal policy {final_policy} and the final '
+          f'performance value is {final_performance}.')
 
 
 if __name__ == '__main__':
-    monte_carlo()
+    qlearning()
